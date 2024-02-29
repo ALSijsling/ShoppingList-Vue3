6c8a78bceacd5da36ef85e7f51de0d114fe9d70b
ALSijsling <125027260+ALSijsling@users.noreply.github.com>
2024-02-29 12:19:33 +0100

    added component for groceries table

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
-  import {ref, computed} from 'vue'
-
+  import {ref} from "vue"
+  import GroceriesTable from "./components/GroceriesTable.vue"
+  
   let id = 0
 
   const groceries = ref([
@@ -9,34 +10,8 @@
     {id: id++, name: "Krettebollen", price: 1.20, cost: ""},
     {id: id++, name: "Noten", price: 2.99, cost: ""}
   ])
-
-  const grandTotal = computed(() => {
-    return groceries.value.reduce((acc, item) => (acc = acc + item.cost * item.price), 0);
-  });
-
 </script>
 
 <template>
-  <table>
-    <thead class="border-b-2 border-slate-900 border-solid">
-      <tr class="font-bold">
-        <td class="px-4 py-2">Product</td>
-        <td class="px-4 py-2">Price</td>
-        <td class="px-4 py-2">Number</td>
-        <td class="px-4 py-2">Subtotal</td>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="product in groceries" :key="groceries.id">
-        <td class="px-4 py-2">{{ product.name }}</td>
-        <td class="px-4 py-2">{{ product.price }}</td>
-        <td><input v-model="product.cost" type="number" value="0" min="0" class="border border-slate-500 border-solid rounded-md pl-2"></td>
-        <td class="px-4 py-2">{{ (product.price * product.cost).toFixed(2) }}</td>
-      </tr>
-      <tr class="border-t border-slate-900 border-solid">
-        <td class="px-4 py-2 font-bold" colspan="3">Total</td>
-        <td class="px-4 py-2">{{ grandTotal.toFixed(2) }}</td>
-      </tr>
-    </tbody>
-  </table>
+  <GroceriesTable :groceries="groceries" />
 </template>
---
+++ src/components/GroceriesTable.vue
@@ -0,0 +1,35 @@
+<script setup>
+  import {ref, computed} from "vue"
+  
+  const props = defineProps(["groceries"])
+  const groceries = ref(props.groceries)
+
+  const grandTotal = computed(() => {
+    return groceries.value.reduce((acc, item) => (acc = acc + item.cost * item.price), 0);
+  });
+</script>
+
+<template>
+    <table>
+        <thead class="border-b-2 border-slate-900 border-solid">
+        <tr class="font-bold">
+            <td class="px-4 py-2">Product</td>
+            <td class="px-4 py-2">Price</td>
+            <td class="px-4 py-2">Number</td>
+            <td class="px-4 py-2">Subtotal</td>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="product in groceries" :key="groceries.id">
+            <td class="px-4 py-2">{{ product.name }}</td>
+            <td class="px-4 py-2">&euro; {{ product.price.toFixed(2) }}</td>
+            <td><input v-model="product.cost" type="number" value="0" min="0" class="border border-slate-500 border-solid rounded-md pl-2"></td>
+            <td class="px-4 py-2">&euro; {{ (product.price * product.cost).toFixed(2) }}</td>
+        </tr>
+        <tr class="border-t border-slate-900 border-solid">
+            <td class="px-4 py-2 font-bold" colspan="3">Total</td>
+            <td class="px-4 py-2">&euro; {{ grandTotal.toFixed(2) }}</td>
+        </tr>
+        </tbody>
+    </table>
+</template>
