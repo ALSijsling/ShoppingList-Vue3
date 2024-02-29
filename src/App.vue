ea84d950b5952eb7e2815369a715c4dfba28e04a
ALSijsling <125027260+ALSijsling@users.noreply.github.com>
2024-02-29 11:39:28 +0100

    added products subtotal and grandtotal

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <script setup>
-  import {ref} from 'vue'
+  import {ref, computed} from 'vue'
 
   let id = 0
 
   const groceries = ref([
-    {id: id++, name: "Brood", price: 1.00},
-    {id: id++, name: "Broccoli", price: 0.99},
-    {id: id++, name: "Krettebollen", price: 1.20},
-    {id: id++, name: "Noten", price: 2.99}
+    {id: id++, name: "Brood", price: 1.00, cost: ""},
+    {id: id++, name: "Broccoli", price: 0.99, cost: ""},
+    {id: id++, name: "Krettebollen", price: 1.20, cost: ""},
+    {id: id++, name: "Noten", price: 2.99, cost: ""}
   ])
+
+  const grandTotal = computed(() => {
+    return groceries.value.reduce((acc, item) => (acc = acc + item.cost * item.price), 0);
+  });
+
 </script>
 
 <template>
@@ -25,12 +30,12 @@
       <tr v-for="product in groceries" :key="groceries.id">
         <td class="px-4 py-2">{{ product.name }}</td>
         <td class="px-4 py-2">{{ product.price }}</td>
-        <td><input id="number" type="number" value="0" min="0" class="border border-slate-500 border-solid rounded-md pl-2"></td>
-        <td class="px-4 py-2">0</td>
+        <td><input v-model="product.cost" type="number" value="0" min="0" class="border border-slate-500 border-solid rounded-md pl-2"></td>
+        <td class="px-4 py-2">{{ (product.price * product.cost).toFixed(2) }}</td>
       </tr>
       <tr class="border-t border-slate-900 border-solid">
         <td class="px-4 py-2 font-bold" colspan="3">Total</td>
-        <td class="px-4 py-2">0</td>
+        <td class="px-4 py-2">{{ grandTotal.toFixed(2) }}</td>
       </tr>
     </tbody>
   </table>
